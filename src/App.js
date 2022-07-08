de2926177c738a40c8a33158405f54eb538d9252
Arthur Reimus <arthurreimus1623@gmail.com>
2022-07-08 20:04:56 +0800

    Add pokemon information cards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { nanoid } from "nanoid";
+import Pokemon from "./components/Pokemon";
 
-function App() {
+//Review Stage:  Show All Pokemon's Name and Type
+//Start Game
+//Shuffle Pokemons
+//Quiz Stage:
+// - Ask Pokemon Name
+// - Ask Pokemon Type
+// - Record Score & High Score
+
+//CSS
+//Transition Cards off screen
+export default function App() {
+  const [pokemons, setPokemons] = useState([]);
+  // const [score, setScore] = useState(0);
+  // const [stage, setStage] = useState(0);
+
+  useEffect(() => {
+    const pokemonArray = [
+      "charmander",
+      "squirtle",
+      "wartortle",
+      "pikachu",
+      "machamp",
+      "graveler",
+      "mew",
+      "dragonite",
+      "rattata",
+      "butterfree",
+    ];
+
+    async function getPokemon(pokemon) {
+      const response = await fetch(
+        `https://pokeapi.co/api/v2/pokemon/${pokemon}`,
+        { mode: "cors" }
+      );
+      const data = await response.json();
+      setPokemons((prevPokemons) => {
+        return [
+          ...prevPokemons,
+          {
+            id: nanoid(),
+            name: data.species.name,
+            types: {
+              mainType: data.types[0].type.name,
+              secondaryType:
+                data.types.length > 1 ? data.types[1].type.name : null,
+            },
+          },
+        ];
+      });
+    }
+
+    for (let i = 0; i < pokemonArray.length; i++) {
+      getPokemon(pokemonArray[i]);
+    }
+  }, []);
+
+  const pokemonElements = pokemons.map((pokemon) => {
+    return <Pokemon pokemon={pokemon} />;
+  });
+
+  console.log(pokemons);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="row">
+        <div className="container__logo">
+          <img src={require(`./images/logo.png`)} alt="logo" className="logo" />
+        </div>
+        <h1>Memory Game</h1>
       </header>
+      <main>
+        <div className="container__card">{pokemonElements}</div>
+      </main>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Pokemon(props) {
+  return (
+    <div className="col card card__pokemon">
+      {/* `./images/${props.name}.png` */}
+      <div className="card__image">
+        <img
+          src={require(`../images/${props.pokemon.name}.png`)}
+          alt="Card experiences"
+        />
+      </div>
+
+      <p className="pokemon__name">{props.pokemon.name}</p>
+      <div className="row card__pokemon--type">
+        <p className={`pokemon__type ${props.pokemon.types.mainType}`}>
+          {props.pokemon.types.mainType}
+        </p>
+        {props.pokemon.types.secondaryType && (
+          <p className={`pokemon__type ${props.pokemon.types.secondaryType}`}>
+            {props.pokemon.types.secondaryType}
+          </p>
+        )}
+      </div>
+    </div>
+  );
+}
