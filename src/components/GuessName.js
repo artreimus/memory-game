d8fc32d648979a605272d98fe072944a2698dcda
Arthur Reimus <arthurreimus1623@gmail.com>
2022-07-11 19:45:37 +0800

    Refactored and removed

--- src/components/GuessName.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function GuessName(props) {
-  return (
-    <div className="row">
-      <div className="memorizepage">{props.randomPokemonElement}</div>;
-    </div>
-  );
-}
